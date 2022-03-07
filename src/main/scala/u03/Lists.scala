70b5c62dceea6b653edea8ec32f5a7d1669e12cb
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-03-07 21:50:05 +0100

    prepare lab-3

---
+++ src/main/scala/u03/Lists.scala
@@ -0,0 +1,29 @@
+package u03
+
+object Lists extends App:
+
+  // A generic linkedlist
+  enum List[E]:
+    case Cons(head: E, tail: List[E])
+    case Nil()
+
+  // a companion object (i.e., module) for List
+  object List:
+
+    def sum(l: List[Int]): Int = l match
+      case Cons(h, t) => h + sum(t)
+      case _ => 0
+
+    def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
+      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
+      case Nil() => Nil()
+
+    def filter[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
+      case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
+      case Cons(_, t) => filter(t)(pred)
+      case Nil() => Nil()
+
+  val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
+  println(List.sum(l)) // 60
+  import List._
+  println(sum(map(filter(l)(_ >= 20))(_ + 1))) // 21+31 = 52
