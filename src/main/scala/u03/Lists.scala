10a8c840d7dcff1d5f54e03c315b304376bc6c36
Elena Yan <elena.yan@studio.unibo.it>
2022-03-14 12:09:53 +0100

    es lab

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -13,14 +13,34 @@ object Lists extends App:
       case Cons(h, t) => h + sum(t)
       case _ => 0
 
-    def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
-      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
-      case Nil() => Nil()
+//    def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
+//      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
+//      case Nil() => Nil()
+
+    def map[A, B](l: List[A])(mapper: A => B): List[B] = flatMap(l)(v => Cons(mapper(v), Nil()))
 
     def filter[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
       case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
       case Cons(_, t) => filter(t)(pred)
       case Nil() => Nil()
+
+    def drop[A](l: List[A], n: Int): List[A] = l match {
+      case Cons(h, t) => n match {
+        case 1 => t
+        case _ => drop(t, n - 1)
+      }
+      case _ => Nil()
+    }
+
+    def append[A](left: List[A], right: List[A]): List[A] = left match {
+      case Cons(h, t) => Cons(h, append(t, right))
+      case Nil() => right
+    }
+
+    def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = l match {
+      case Cons(h, t) => append(f(h), flatMap(t)(f))
+      case _ => Nil()
+    }
 
   val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
   println(List.sum(l)) // 60
--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -20,3 +20,16 @@ class ListTest:
   @Test def testFilter() =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_ >= 20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_ != 20))
+
+  @Test def testDrop() =
+    assertEquals(Cons (20, Cons (30, Nil ())), drop(l, 1))
+    assertEquals(Cons (30, Nil ()), drop(l, 2))
+    assertEquals(Nil (), drop(l, 5))
+
+  @Test def testAppend() =
+    val tail = Cons(40, Nil())
+    assertEquals(Cons (10, Cons (20, Cons (30, Cons (40, Nil ())))), append(l, tail))
+
+  @Test def testFlatMap() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
+    assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons (32, Nil ())))))), flatMap(l)(v => Cons(v + 1, Cons(v + 2, Nil()))))
